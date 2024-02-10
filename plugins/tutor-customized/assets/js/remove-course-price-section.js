a6c16cba111e1744aed98d669781e885100c834f
Golden Chicken <chicken931205@outlook.com>
2024-02-10 05:21:11 -0800

    fix small

--- plugins/tutor-customized/assets/js/remove-course-price-section.js
+++ plugins/tutor-customized/assets/js/remove-course-price-section.js
@@ -5,6 +5,6 @@
         $("div.tutor-row.tutor-mb-32 > div.tutor-col-12.tutor-mb-12 > label.tutor-fs-6.tutor-fw-medium.tutor-color-black").html("");
         $("div.tutor-row.tutor-mb-32 > div.tutor-col-12.tutor-mb-12 > label.tutor-fs-6.tutor-fw-medium.tutor-color-black").html("Total Sessions");
 
-        $("div.tutor-row.tutor-mb-32 > div.tutor-col-6.tutor-col-sm-4.tutor-col-md-3:nth-child(2)").css({'display': 'none'});
+        $("div.tutor-row.tutor-mb-32 > div.tutor-col-6.tutor-col-sm-4.tutor-col-md-3:nth-child(3)").css({'display': 'none'});
     });
 })(jQuery);
